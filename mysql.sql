2c43290e11298711def6f77524a8f2784c09e89c
blacktea77777 <107b11513@mailst.cjcu.edu.tw>
2020-11-03 16:04:12 +0800

    5

--- mysql.sql
+++ mysql.sql
@@ -8,8 +8,73 @@ CREATE TABLE `goods` (
   PRIMARY KEY (`goods_sn`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE `bill` (
+  `bill_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '帳單編號',
+  `user_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '購買人',
+  `bill_total` mediumint(8) UNSIGNED NOT NULL COMMENT '總金額',
+  `bill_date` datetime NOT NULL COMMENT '購買日期',
+  `bill_status` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '處理狀態'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 傾印資料表的資料 `bill`
+--
+
+INSERT INTO `bill` (`bill_sn`, `user_sn`, `bill_total`, `bill_date`, `bill_status`) VALUES
+(1, 2, 1, '2020-11-03 07:07:34', NULL),
+(2, 1, 1, '2020-11-03 07:08:55', '已出貨');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `bill_detail`
+--
+
+CREATE TABLE `bill_detail` (
+  `bill_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '帳單編號',
+  `goods_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '商品編號',
+  `goods_amount` tinyint(3) UNSIGNED NOT NULL COMMENT '購買數量',
+  `goods_total` mediumint(8) UNSIGNED NOT NULL COMMENT '小計'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 傾印資料表的資料 `bill_detail`
+--
+
+INSERT INTO `bill_detail` (`bill_sn`, `goods_sn`, `goods_amount`, `goods_total`) VALUES
+(1, 2, 1, 1),
+(2, 2, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `goods`
+--
+
+CREATE TABLE `goods` (
+  `goods_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '商品編號',
+  `goods_title` varchar(255) NOT NULL COMMENT '商品名稱',
+  `goods_content` text NOT NULL COMMENT '商品說明',
+  `goods_price` mediumint(8) UNSIGNED NOT NULL COMMENT '商品價錢',
+  `goods_counter` smallint(5) UNSIGNED NOT NULL COMMENT '人氣',
+  `goods_date` datetime NOT NULL COMMENT '上架日期'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 傾印資料表的資料 `goods`
+--
+
+INSERT INTO `goods` (`goods_sn`, `goods_title`, `goods_content`, `goods_price`, `goods_counter`, `goods_date`) VALUES
+(2, 'dog', '<p>dog</p>\r\n', 1, 5, '2020-11-03 06:53:11');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `users`
+--
+
 CREATE TABLE `users` (
-  `user_sn` mediumint(8) unsigned NOT NULL AUTO_INCREMENT COMMENT '使用者編號',
+  `user_sn` mediumint(8) UNSIGNED NOT NULL COMMENT '使用者編號',
   `user_name` varchar(255) NOT NULL COMMENT '使用者姓名',
   `user_id` varchar(255) NOT NULL COMMENT '使用者帳號',
   `user_passwd` varchar(255) NOT NULL COMMENT '使用者密碼',
@@ -19,25 +84,69 @@ CREATE TABLE `users` (
   `user_zip` varchar(255) NOT NULL COMMENT '使用者郵遞區號',
   `user_county` varchar(255) NOT NULL COMMENT '使用者縣市',
   `user_district` varchar(255) NOT NULL COMMENT '使用者鄉鎮市區',
-  `user_address` varchar(255) NOT NULL COMMENT '使用者地址',
-  PRIMARY KEY (`user_sn`),
-  UNIQUE KEY `user_id` (`user_id`)
+  `user_address` varchar(255) NOT NULL COMMENT '使用者地址'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `bill` (
-  `bill_sn` mediumint(8) unsigned NOT NULL AUTO_INCREMENT COMMENT '帳單編號',
-  `user_sn` mediumint(8) unsigned NOT NULL COMMENT '購買人',
-  `bill_total` mediumint(8) unsigned NOT NULL COMMENT '總金額',
-  `bill_date` datetime NOT NULL COMMENT '購買日期',
-  `bill_status` varchar(255) NOT NULL COMMENT '處理狀態',
-  PRIMARY KEY (`bill_sn`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+--
+-- 傾印資料表的資料 `users`
+--
 
+INSERT INTO `users` (`user_sn`, `user_name`, `user_id`, `user_passwd`, `user_email`, `user_sex`, `user_tel`, `user_zip`, `user_county`, `user_district`, `user_address`) VALUES
+(1, 'rrrrrrrr', 'www', '$2y$10$GPH45JbE.RKDQ/gZ1Vk5dOGr9GnGs3QyUWiMDr7.ZlDr/og3GQ5Tm', 'yy@gmail.com', '先生', '099999999', '6', '台東縣', '臺東市', 'rrrr'),
+(2, 'jj', 'qqq', '$2y$10$tni/QT5ISNuoPUVir76ETOWSaFCnWx1wb4ya/mLeEdCqdyxwFJ0M2', '23@gmail.com', '先生', '096666666', '924', '屏東縣', '崁頂鄉', '22');
 
-CREATE TABLE `bill_detail` (
-  `bill_sn` mediumint(8) unsigned NOT NULL COMMENT '帳單編號',
-  `goods_sn` mediumint(8) unsigned NOT NULL COMMENT '商品編號',
-  `goods_amount` tinyint(3) unsigned NOT NULL COMMENT '購買數量',
-  `goods_total` mediumint(8) unsigned NOT NULL COMMENT '小計',
-  PRIMARY KEY (`bill_sn`,`goods_sn`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`bill_sn`);
+
+--
+-- 資料表索引 `bill_detail`
+--
+ALTER TABLE `bill_detail`
+  ADD PRIMARY KEY (`bill_sn`,`goods_sn`);
+
+--
+-- 資料表索引 `goods`
+--
+ALTER TABLE `goods`
+  ADD PRIMARY KEY (`goods_sn`);
+
+--
+-- 資料表索引 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_sn`),
+  ADD UNIQUE KEY `user_id` (`user_id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `bill`
+--
+ALTER TABLE `bill`
+  MODIFY `bill_sn` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '帳單編號', AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `goods`
+--
+ALTER TABLE `goods`
+  MODIFY `goods_sn` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '商品編號', AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_sn` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '使用者編號', AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
